46b2dad29dbe6345107fec2f9c908cbe73e6f1fe
Chirag <chhirag920@gmail.com>
2025-11-30 00:42:55 +0530

    pushing my code

--- src/utils/LocalTime.tsx
+++ src/utils/LocalTime.tsx
@@ -4,6 +4,7 @@ const LocalTime = () => {
   const [time, setTime] = useState(new Date())
 
   useEffect(() => {
+    // This will only run on the client side
     const timer = setInterval(() => {
       setTime(new Date())
     }, 1000)
@@ -11,8 +12,12 @@ const LocalTime = () => {
     return () => clearInterval(timer)
   }, [])
 
+  if (!time) {
+    return null // Return null during SSR and initial render
+  }
+
   return (
-    <div>
+    <div suppressHydrationWarning>
       {time.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}
     </div>
   )
